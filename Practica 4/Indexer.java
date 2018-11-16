7382ecba8dca597e029d265134cbbec3303e72ac
Miguel Ángel <torresma.1996@gmail.com>
2018-11-16 10:55:28 +0100

    Runnable, with error while reading csv

--- Practica 4/Indexer.java
+++ Practica 4/Indexer.java
@@ -1,17 +1,34 @@
-import org.apache.lucene.analysis.Analyzer ;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.Field.Store;
 
 public class Indexer{
 	String indexPath = "./index";
-	String docPath = "./DataSet";
+	String[] docPath = {"./source/Questions.csv", "./source/Answers.csv", "./source/Tags.csv"};
 	boolean create = true;
+	private Reader reader = new Reader();
 	private IndexWriter writer ;
 	
 
 	Indexer(){
-	// TO DO
-
-
 
 	}
 
@@ -20,21 +37,26 @@ public class Indexer{
 		Similarity similarity = new ClassicSimilarity(); // BM25 by default
 
 		// Create an analyzer per field, and set default as WhitespaceAnalyzer
-		PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrappwer(new WhitespaceAnalyzer(), analyzerSet());
+		PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrapper(new WhitespaceAnalyzer(), analyzerSet());
 
-		IndiceSimple baseline = new IndiceSimple() ;
+		Indexer baseline = new Indexer() ;
 
-		baseline.configureIndex (analyzer, similarity);
+		try{
+			baseline.configureIndex (analyzer, similarity);
+		}catch( IOException e ){
+			System.out.println("Error configuring the index.");
+		}		
 		baseline.indexDocs() ;
 		baseline.close();
 	}
 
 
-	public Map<String, Analyzer> analyzerSet(){
-	// TO DO
+	public static Map<String, Analyzer> analyzerSet(){
+		Map<String, Analyzer> res = new HashMap<String, Analyzer>();
 
 
 
+		return res;
 	}
 
 
@@ -54,39 +76,65 @@ public class Indexer{
 
 
 	public void indexDocs (){
-		for(elementos d : docPath){
-			String cadena = leerDocumento (d);
-			Document doc = new Document ();
-			
-			// Extract ID from String
-			Integer start = . . . 
-			Integer end = . . . 
-			String aux = cadena.substring (start,end);
-			Integer valor = Integer.decode(aux);
-			// Store field on Lucene doc
-			doc.add ( new IntPoint("ID",valor));
-			doc.add ( new StoredField("ID", valor));
-
-			// Extract Body from string
-			start =
-			end = 
-			Stringcuerpo = cadena.substring(start,end);
-			// Store field on Lucene doc
-			doc.add(new TextField( "Body", body, Field.Store.YES));
-
-
-			// Insert doc in Index
-			writer.addDocument(doc);
-
-			// If already exist, update with:
-			// writer.updateDocument(new Term("ID", valor.toString()),doc);
+		List<String[]> questions = null;
+		try{
+			questions = reader.read(docPath[0],"Questions");
+		}catch(IOException e){
+			System.out.println(e.getMessage());
 		}
+		System.out.println(questions);
+
+		/*indexQuestions(cadena);
+		cadena = reader.read(docPath[1],"Answers");
+		indexQuestions(cadena);
+		cadena = reader.read(docPath[2],"Tags");
+		indexQuestions(cadena);*/
 	}
+
+
+	public void indexQuestions(String cadena){
+		Document doc = new Document ();
+			
+		// Extract ID from String
+		Integer start = 0;
+		Integer end = 0; 
+		String aux = cadena.substring (start,end);
+		Integer valor = Integer.decode(aux);
+		// Store field on Lucene doc
+		doc.add ( new IntPoint("ID",valor));
+		doc.add ( new StoredField("ID", valor));
+
+		// Extract Body from string
+		start = 0;
+		end = 0;
+		String body = cadena.substring(start,end);
+		// Store field on Lucene doc
+		doc.add(new TextField( "Body", body, Store.YES));
+
+
+		// Insert doc in Index
+		try{
+			writer.addDocument(doc);
+		}catch( IOException e ){
+			System.out.println("Error adding document to index.");
+		}	
+
+
+		// If already exist, update with:
+		// writer.updateDocument(new Term("ID", valor.toString()),doc);
+	}
+	public void indexAnswers(String cadena){
+
+	}
+	public void indexTags(String cadena){
+
+	}
+
 
 	public void close ( ) {
 		try {
 			writer.commit ( ) ;
-			writer . close () ;
+			writer.close () ;
 		}catch ( IOException e ) {
 			System.out.println("Error closing the index.");
 		}
--- Practica 4/Reader.java
+++ Practica 4/Reader.java
@@ -1,6 +1,44 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
 
+//import org.jsoup.nodes.Document;
+//import org.jsoup.Jsoup;
+import com.opencsv.CSVReader;
 
 public class Reader{
+	Reader(){}
+
+	public List<String[]> read(String path, String type) throws IOException {
+		List<String[]> res = new LinkedList<String[]> ();
+		CSVReader csvReader = new CSVReader(new FileReader(path), ',');
+		Scanner scanner = new Scanner(new File(path));
+		scanner.useDelimiter(",");
+		String[] aux;
 
 
+		if(type == "Questions"){
+			//res = csvReader.readAll() ;		
+			
+			
+			while((aux = csvReader.readNext()) != null){
+				//System.out.println("_________________________________");
+				//System.out.println(aux[0]);
+				res.add(aux);
+			}
+
+			/*while(scanner.hasNext()){
+				System.out.print(scanner.next()+"|-|-|");
+			}*/
+		}else if(type == "Answers"){
+			
+		}else{
+
+		}
+
+		return res;
+	}
 }
