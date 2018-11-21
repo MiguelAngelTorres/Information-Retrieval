6765f6367f12809f5175e8c53a6dae6054e14ca8
MiguelAngelTorres <torresma.1996@gmail.com>
2018-11-21 20:34:32 +0100

    First simple interface (runnable but not functional)

--- Practica 4/SearchInterface.java
+++ Practica 4/SearchInterface.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
 import java.io.IOException;
+
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.Query;
+
 
 public class SearchInterface{
 	public static Searcher searcher;
+	public ArrayList<String> clauses;
+	public ArrayList<String> filters;
+	public ArrayList<String> order;
+	public Integer maxHits;
+	public boolean firstSearch;
+
+///////////////// -----------------------   SEARCH METHODS  ---------------------------- /////////
 
 	SearchInterface(String path){
 		try{		
@@ -9,17 +21,65 @@ public class SearchInterface{
 		} catch ( IOException e){
 			System.out.println(e.getMessage());
 		}
+		clauses = new ArrayList<String>();
+		maxHits = 5;
+		firstSearch = true;
 	}
 
+	public void resetSearch(){
+		searcher.resetSearch();
+		clauses.clear();
+		filters.clear();
+		order.clear();
+	}
+
+	// words cannot be empty
+	public void addSearchByTitle(String[] words, boolean obligatory) throws IOException {
+		String clause = "The word(s)";
+		if(obligatory){
+			searcher.addSearchByTitle(words,BooleanClause.Occur.MUST);
+			for(String word : words){
+				clause += " [" + word + "]";
+ 			}
+			clause += " must appear on the title.";
+			clauses.add(clause);
+		}else{
+			searcher.addSearchByTitle(words,BooleanClause.Occur.SHOULD);
+			for(String word : words){
+				clause += " [" + word + "]";
+ 			}
+			clause += " should appear on the title.";
+			clauses.add(clause);
+		}
+	}
+
+	public void executeSearch(){
+		try{
+			searcher.executeSearch(maxHits);
+		} catch( IOException e ){
+			System.out.println("Error while executing search: " + e.getMessage());
+		}
+	}
+
+
+///////////////// ----------------------   INTEFACE METHODS  ---------------------------- /////////
+
+	public void requestClause(){
+		System.out.println("\nPlease, input your's number choice:\n" +
+									"  [1] - Add word restriction"
+		);
+	}
+
+
+
+
+///////////////// -----------------------   MAIN METHOD  ---------------------------- ///////////
+	
 	public static void main ( String[] args ) {
 		SearchInterface In = new SearchInterface(args[0]);
 		
-		String[] query = {"vector"};
-		try{
-			searcher.searchByTitle(query,5);
-		} catch (IOException e){
-			System.out.println("Error while searching: " + e.getMessage());
-		}
+		In.requestClause();
+
 
 	}
 
--- Practica 4/Searcher.java
+++ Practica 4/Searcher.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.nio.file.Paths;
 import java.io.IOException;
 
@@ -14,36 +15,58 @@ import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.SortField;
+import org.apache.lucene.search.MatchAllDocsQuery;
 
 public class Searcher{
 	IndexSearcher searcher;
-	
+	public ArrayList<BooleanClause> clauses;
+	public ArrayList<SortField> order;	
 	
 	Searcher(String path) throws IOException {
 		Directory dir = FSDirectory.open(Paths.get(path));
 	 	IndexReader reader = DirectoryReader.open(dir);
 		searcher = new IndexSearcher(reader);	
+		clauses = new ArrayList<BooleanClause>();
+		order = new ArrayList<SortField>();
 	}
 
+	public void resetSearch(){
+		clauses.clear();
+		order.clear();
+	}
 
-	public void searchByTitle(String[] words, int maxres) throws IOException {
+	public void addSearchByTitle(String[] words, BooleanClause.Occur oblig) throws IOException {
 		Query q;
 		BooleanClause bc;
-		BooleanQuery bq;
-		BooleanQuery.Builder bqbuilder = new BooleanQuery.Builder();
 	
 		for(String word : words){
 			q = new TermQuery(new Term("Title", word));
-			bc = new BooleanClause(q, BooleanClause.Occur.SHOULD);
+			bc = new BooleanClause(q, oblig);
+			clauses.add(bc);
+		}
+	}
+
+	// if no clauses are given, all the questions are returned
+	public void executeSearch(int maxres) throws IOException {
+		BooleanQuery bq;
+		BooleanQuery.Builder bqbuilder = new BooleanQuery.Builder();
+
+		if(clauses.isEmpty()){
+			Query q = new MatchAllDocsQuery();
+			BooleanClause bc = new BooleanClause(q, BooleanClause.Occur.SHOULD);
+			clauses.add(bc);
+		}
+
+		for(BooleanClause bc : clauses){
 			bqbuilder.add(bc);
 		}
 		bq = bqbuilder.build();
 
-		System.out.println(bq.toString());
 		TopDocs tdocs = searcher.search(bq,maxres);
 		printDocs(tdocs);
-	}
 
+	}
 
 	public void printDocs(TopDocs docs) throws IOException {
 		System.out.println(docs.totalHits + " resultados relacionados.\n");
